dc0ec1bce1be4d43f321a0ea3ffe1b9af28fc91e
adiamitlevy1122 <adiamitlevy@gmail.com>
2026-04-29 15:30:10 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sugar-tracker-v13';
+const CACHE = 'sugar-tracker-v14';
 const SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
